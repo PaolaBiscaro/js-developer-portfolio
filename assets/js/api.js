e03386888ddd70ac7304e055601ce093153f9c10
Paola Biscaro <paolabiscaroper@gmail.com>
2025-07-13 16:03:46 -0300

    Correção JSON - caminho do arquivo

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/PaolaBiscaro/js-developer-portfolio/refs/heads/main/js-developer-portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/PaolaBiscaro/js-developer-portfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
